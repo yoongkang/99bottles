17a099b93ea2bf076c9677081e3dae90c8df17ea
Yoong Kang Lim <yoongkang.lim@gmail.com>
2018-05-31 21:36:29 +1000

    2nd and 3rd test

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,6 @@
 class Bottles
   def verse(num)
       "#{num} bottles of beer on the wall, #{num} bottles of beer.\n"\
-      "Take one down and pass it around, #{num-1} bottles of beer on the wall.\n"
+      "Take one down and pass it around, #{num-1} bottle#{num > 2 ? "s" : ""} of beer on the wall.\n"
   end
 end
