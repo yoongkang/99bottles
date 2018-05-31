265c74e2732f527625aad183aeca88d365fc96f7
Yoong Kang Lim <yoongkang.lim@gmail.com>
2018-05-31 21:39:41 +1000

    Make it a little smarter

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,15 @@
 class Bottles
+
   def verse(num)
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\n"\
-      "Take one down and pass it around, #{num-1} bottle#{num > 2 ? "s" : ""} of beer on the wall.\n"
+    [firstline(num), secondline(num)].map { |s| s + "\n"}.join
+  end
+
+  def firstline(num)
+    "#{num} bottles of beer on the wall, #{num} bottles of beer."
+  end
+
+  def secondline(num)
+    s = num > 2 ? "s" : ""
+    "Take one down and pass it around, #{num-1} bottle#{s} of beer on the wall."
   end
 end
