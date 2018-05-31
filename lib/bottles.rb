427c0a6f903080ce02e4d936bcd9aa42c1432118
Yoong Kang Lim <yoongkang.lim@gmail.com>
2018-05-31 22:46:13 +1000

    redundant method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -14,25 +14,15 @@ class Bottles
   end
 
   def firstline(num)
-    num_bottles = current_bottle(num)
+    num_bottles = remaining_bottle(num)
     "#{num_bottles} of beer on the wall, #{num_bottles.downcase} of beer."
   end
 
   def secondline(num)
     if num > 0
-      "Take #{num == 1 ? "it": "one"} down and pass it around, #{remaining_bottle(num - 1)} of beer on the wall."
+      "Take #{num == 1 ? "it": "one"} down and pass it around, #{remaining_bottle(num - 1).downcase} of beer on the wall."
     else
       "Go to the store and buy some more, 99 bottles of beer on the wall."
-    end
-  end
-
-  def current_bottle(num)
-    if num == 1
-      "1 bottle"
-    elsif num == 0
-      "No more bottles"
-    else
-      "#{num} bottles"
     end
   end
 
@@ -40,7 +30,7 @@ class Bottles
     if num == 1
       "1 bottle"
     elsif num == 0
-      "no more bottles"
+      "No more bottles"
     else
       "#{num} bottles"
     end
